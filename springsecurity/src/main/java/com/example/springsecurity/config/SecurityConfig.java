95710a7e6c988c8f58362d0e06319b135757c7a0
Nishaanth <nishaanth.it21@bitsathy.ac.in>
2025-08-08 14:41:40 +0530

    final security commit

--- springsecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ springsecurity/src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -3,19 +3,26 @@ package com.example.springsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	
+	@Autowired
+	private JwtFilter jwtFilter;
 	
 	@Autowired
 	private UserDetailsService userDetailsService;
@@ -29,19 +36,25 @@ public class SecurityConfig {
 
 		return http
 			    .csrf(csrf -> csrf.disable()) // Disable CSRF protection
-			    .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()) // All requests need authentication
+			    .authorizeHttpRequests(auth -> auth.requestMatchers("register","login").permitAll().anyRequest().authenticated()) // All requests need authentication
 			    .httpBasic(Customizer.withDefaults()) // Use HTTP Basic authentication
 			    .sessionManagement(session -> session
 			        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // No server-side sessions (stateless)
+			    .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
 			    .build();
-
+ 
 	}
 	@Bean
 	public AuthenticationProvider authenticationProvider()
 	{
 		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
 		provider.setUserDetailsService(userDetailsService);
 		return provider;
+	}
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception
+	{
+		return config.getAuthenticationManager();
 	}
 }
---
+++ springsecurity/src/main/java/com/example/springsecurity/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.springsecurity.model.Users;
+import com.example.springsecurity.service.UserService;
+
+@RestController
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@PostMapping("/register")
+	public Users register(@RequestBody Users user)
+	{
+		return service.register(user);
+	}
+	@PostMapping("/login")
+	public String login(@RequestBody Users user)
+	{
+		return service.verify(user);
+	}
+}
